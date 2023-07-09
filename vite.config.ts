99d18233c1c28c3f5ea69e1540281d29ecd1d558
Christophe Tiraoui <tuxounet@gmail.com>
2023-07-09 21:43:03 +0200

    fix root

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import { VitePWA } from "vite-plugin-pwa";
 export default defineConfig({
-  base: "/k-desk", 
+  base: "/k-desk",
   plugins: [
     react(),
     VitePWA({
@@ -19,63 +19,53 @@ export default defineConfig({
         start_url: "/",
         icons: [
           {
-            src: "/android-chrome-36x36.png",
+            src: "/k-desk/android-chrome-36x36.png",
             sizes: "36x36",
             type: "image/png",
           },
           {
-            src: "/android-chrome-48x48.png",
+            src: "/k-desk/android-chrome-48x48.png",
             sizes: "48x48",
             type: "image/png",
           },
           {
-            src: "/android-chrome-72x72.png",
+            src: "/k-desk/android-chrome-72x72.png",
             sizes: "72x72",
             type: "image/png",
           },
           {
-            src: "/android-chrome-96x96.png",
+            src: "/k-desk/android-chrome-96x96.png",
             sizes: "96x96",
             type: "image/png",
           },
           {
-            src: "/android-chrome-144x144.png",
+            src: "/k-desk/android-chrome-144x144.png",
             sizes: "144x144",
             type: "image/png",
           },
           {
-            src: "/android-chrome-192x192.png",
+            src: "/k-desk/android-chrome-192x192.png",
             sizes: "192x192",
             type: "image/png",
           },
           {
-            src: "/android-chrome-256x256.png",
+            src: "/k-desk/android-chrome-256x256.png",
             sizes: "256x256",
             type: "image/png",
           },
           {
-            src: "/android-chrome-384x384.png",
+            src: "/k-desk/android-chrome-384x384.png",
             sizes: "384x384",
             type: "image/png",
           },
           {
-            src: "/android-chrome-512x512.png",
+            src: "/k-desk/android-chrome-512x512.png",
             sizes: "512x512",
             type: "image/png",
           },
         ],
         background_color: "#ffffff",
         display: "standalone",
-        share_target: {
-          action: "/share/handle/",
-          enctype: "application/x-www-form-urlencoded",
-          method: "GET",
-          params: {
-            title: "title",
-            text: "text",
-            url: "url",
-          },
-        },
       },
     }),
   ],
